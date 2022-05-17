a6e873406231a8902326a5c2d643c21abc9a9c14
AnnaPeBraz <annap.0103@gmail.com>
2022-05-17 18:36:10 -0300

    javascript

--- 5JavaScript-parcial/Js/index.js
+++ 5JavaScript-parcial/Js/index.js
@@ -1,4 +1,6 @@
-let listToDo = []   
+const dados = localStorage.getItem('lista');
+
+const listToDo = JSON.parse(dados);  
 
 const formIn = document.querySelector('form'); //le o formulario da DOM
 console.log(formIn)
@@ -6,46 +8,60 @@ console.log(formIn)
 const lista = document.querySelector(".todo-list") //le a lista da DOM (ul)
 console.log(lista)
 
+const testeProcura = document.querySelector('.select')
 
 const pressButtonPlus = function(eventSubmit){
     eventSubmit.preventDefault();
 
     //pegar o input da lista
 
-    let addText = formIn.querySelector('#inputText').value; //pega o valor escrito na area de input
+    let addText = formIn.querySelector('#inputText')//.value; //pega o valor escrito na area de input
 
     
 
-    let ok = validacao(addText)
+    let ok = validacao(addText.value)
     
     if (ok == true){   
     
         //criar o elemento tarefa 
-        newTags (item)
-
-        butaoFeito.innerHTML = '<i class="fas fa-check" aria-hidden="true"></i>';
-        // butaoFeito.id = listToDo.length;
-        // const done = () => {
-        //     document.querySelectorAll('.check-button').forEach(() => {
-        //         addEventListener("click", (event) =>{
-        //             console.log("key", event.target.id)
-        //             event.target.previousElementSibling.className = 'todo item check'
-        //         })
-                
-        //     })
-        // }
-        
-        butaoFeito.classList = "check-btn";
-
-
+        let item = {
+            id:listToDo.length + 1,
+            texto: addText,
+            status: "pendente"
+        }
     
+        newItem(addText);
+
+
+
+        let li1 = document.createElement('li');
+        li1.innerHTML = addText.value;
+        li1.classList = "todo-item";
+
+        let butaoFeito = document.createElement('button')
+        butaoFeito.classList = "check-btn";
+        butaoFeito.innerHTML = '<i class="fas fa-check" aria-hidden="true"></i>';
+        butaoFeito.id = listToDo.length;
+        butaoFeito.addEventListener('click', function(b){
+             
+            let botao = b.target;
+            let id = parseInt(botao.id) ;
+ 
+          //  excluir elemento da lista
+            for(let i = 0; i < listToDo.length; i++){
+                if(id == listToDo[i].id){
+                    li1.classList = "todo-item completed";
+                    item.status = "Finalizado"
+                }
+            } console.log(item.status)
+        })
+
         let butaoX = document.createElement('button')
         butaoX.innerHTML = '<i class="fas fa-trash" aria-hidden="true"></i>';
         butaoX.classList = "trash-btn";
         butaoX.id = listToDo.length;
 
         butaoX.addEventListener('click', function(b){
-            //     console.log(butaoFeito.id)
              
             let botao = b.target;
             let id = parseInt(botao.id) ;
@@ -63,7 +79,7 @@ const pressButtonPlus = function(eventSubmit){
           });
     
         let diva = document.createElement('div'); // add elementos a div
-        diva.classList = "todo"
+        diva.classList = "todo";
         diva.id = 'div'+ listToDo.length;
         diva.append(li1);
         diva.append(butaoFeito);
@@ -71,10 +87,12 @@ const pressButtonPlus = function(eventSubmit){
 
         lista.append(diva); //add a div a lista
     
-
-
+        document.querySelector("#inputText").value ='';
+    
 
     }
+
+  
 
 }
 
@@ -82,92 +100,94 @@ formIn.addEventListener('submit', pressButtonPlus);
 
 function validacao(text){
     if(text == ''){ //validar para que o input nao venha vazio
-        alert('precisa colocar uma tarefa');
-     }
-     return false 
-}
+        alert('precisa colocar uma tarefa');  
+    }else{
+        return true;
+    }
+    
+} // ta funcionando
 
 function newItem(texto){
     let item = {
         id:listToDo.length + 1,
-        texto: texto,
+        texto: texto.value,
         status: "pendente"
     }
-    
+
     listToDo.push(item);
-
-    newTags (item);
-
-    newTags (item)
-
-        butaoFeito.innerHTML = '<i class="fas fa-check" aria-hidden="true"></i>';
-        // butaoFeito.id = listToDo.length;
-        // const done = () => {
-        //     document.querySelectorAll('.check-button').forEach(() => {
-        //         addEventListener("click", (event) =>{
-        //             console.log("key", event.target.id)
-        //             event.target.previousElementSibling.className = 'todo item check'
-        //         })
-                
-        //     })
-        // }
         
-        butaoFeito.classList = "check-btn";
+    const dadosJson = JSON.stringify(listToDo); //converter em json
+    localStorage.setItem('lista', dadosJson);
 
+} // ta funcionando
 
-    
-        let butaoX = document.createElement('button')
-        butaoX.innerHTML = '<i class="fas fa-trash" aria-hidden="true"></i>';
-        butaoX.classList = "trash-btn";
-        butaoX.id = listToDo.length;
+function newTags(){
+    let butaoFeito = document.createElement('button')
+    butaoFeito.classList = "check-btn";
+    butaoFeito.innerHTML = '<i class="fas fa-check" aria-hidden="true"></i>';
 
-        butaoX.addEventListener('click', function(b){
-            //     console.log(butaoFeito.id)
+    let butaoX = document.createElement('button')
+    butaoX.innerHTML = '<i class="fas fa-trash" aria-hidden="true"></i>';
+    butaoX.classList = "trash-btn";
+    butaoX.id = listToDo.length;
+
+    butaoX.addEventListener('click', function(b){
              
-            let botao = b.target;
-            let id = parseInt(botao.id) ;
-     
-              //  excluir elemento da lista
-            for(let i = 0; i < listToDo.length; i++){
-                if(id == listToDo[i].id){
-                     listToDo.splice(i,1);
-                 }
-            }
+        let botao = b.target;
+        let id = parseInt(botao.id) ;
+ 
+          //  excluir elemento da lista
+        for(let i = 0; i < listToDo.length; i++){
+            if(id == listToDo[i].id){
+                 listToDo.splice(i,1);
+             }
+        }
 
-            //remover elemento da tela
-            let divRemovida = document.querySelector('#div'+id)
-            lista.removeChild(divRemovida);
-          });
-    
-        let diva = document.createElement('div'); // add elementos a div
-        diva.classList = "todo"
-        diva.id = 'div'+ listToDo.length;
-        diva.append(li1);
-        diva.append(butaoFeito);
-        diva.append(butaoX);
+        //remover elemento da tela
+        let divRemovida = document.querySelector('#div'+id)
+        lista.removeChild(divRemovida);
+      });
+       diva = document.createElement('div'); // add elementos a div
+      diva.classList = "todo"
+      diva.id = 'div'+ listToDo.length;
+      diva.append(li1);
+      diva.append(butaoFeito);
+      diva.append(butaoX);
 
-        lista.append(diva); //add a div a lista
-    
-    }
+      lista.append(diva); //add a div a lista
+} // nops
 
-}
 
-function newTags (item){
-    
-    
-    let li1 = document.createElement('li');
-        li1.innerHTML = item.texto.value;
-        li1.classList = "todo-item";
-
-        let butaoFeito = document.createElement('button')
-
-}
-// function pressButtonOk(){
-//     const listOk = document.getElementById('inputText').value
-//     alert(addText) 
-// }
-
+function pressButtonOk(butaoFeito){
+    const listOk = butaoFeito.addEventListener('click', function(b){
+             
+        let botao = b.target;
+        let id = parseInt(botao.id) ;
+ 
+          //  excluir elemento da lista
+        for(let i = 0; i < listToDo.length; i++){
+            if(id == listToDo[i].id){
+                console.log('ahoy')
+             }
+        }
+    })
 // function pressButtonTrash(){
 //     const deleteText = document.getElementById('inputText').value
 //     alert(addText) 
-// }
+} // nops
+
+function filter(){
+    let finalizadas = item.filter((item) =>{
+        return item.status === "Finalizado"
+    })
+}
+
+    
+  const filtros = () =>{
+        let filtro1 = document.querySelector(".filter-todo")
+        // let option = 
+        console.log(filtro1)
+    }
+
+
+
